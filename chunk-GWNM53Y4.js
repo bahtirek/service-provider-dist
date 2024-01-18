f7c2b3cde6d87ed0c49863007d87e82a3b037070
bahtirek <bahtirek@gmail.com>
2024-01-17 21:24:52 -0500

    file uploader

---
+++ chunk-GWNM53Y4.js
@@ -0,0 +1,1 @@
+import{Oa as i,Pa as r,V as o,_a as a,fb as p}from"./chunk-O2WMGYX5.js";import"./chunk-JMZ7DUBM.js";var c=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-profile"]],standalone:!0,features:[p],decls:2,vars:0,template:function(n,f){n&1&&(i(0,"p"),a(1,"profile works!"),r())}});let t=e;return t})();export{c as ProfileComponent};
