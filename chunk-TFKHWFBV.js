43b1bc4539c823f72c4e742118cf2b9e524dda64
bahtirek <bahtirek@gmail.com>
2024-01-28 16:50:11 -0500

    change: fileupload

---
+++ chunk-TFKHWFBV.js
@@ -0,0 +1,1 @@
+import"./chunk-JMZ7DUBM.js";var t=[{path:"login",loadComponent:()=>import("./chunk-RX5G2NRJ.js").then(o=>o.LoginComponent)},{path:"registration",loadComponent:()=>import("./chunk-2NONQCYY.js").then(o=>o.RegistrationComponent)},{path:"",redirectTo:"login",pathMatch:"full"}];export{t as AUTH_ROUTES};
