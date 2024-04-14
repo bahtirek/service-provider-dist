fe217792269fae21df31f38b2657507abe8de02d
bahtirek <bahtirek@gmail.com>
2024-04-13 23:46:31 -0400

    change: added client and provider profiles

---
+++ chunk-ECXZ4PCD.js
@@ -0,0 +1,1 @@
+import"./chunk-JMZ7DUBM.js";var t=[{path:"login",loadComponent:()=>import("./chunk-KUNSTIST.js").then(o=>o.LoginComponent)},{path:"registration",loadComponent:()=>import("./chunk-QSXMGQL6.js").then(o=>o.RegistrationComponent)},{path:"",redirectTo:"login",pathMatch:"full"}];export{t as AUTH_ROUTES};
